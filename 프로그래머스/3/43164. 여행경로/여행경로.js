899edd46e0c0b0ba6add7a72864314382f7d27e8
YeojinChoi <yesjin1209@gmail.com>
2024-08-01 09:14:00 +0900

    [level 3] Title: 여행경로, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,33 @@
+// 모든 티켓을 사용하는 것이 목표 dfs 
+function solution(tickets) {
+    const routes = {};
+    
+    // 티켓 정보를 바탕으로 경로를 만듦
+    for (const [from, to] of tickets) {
+        if(!routes[from]) {
+            routes[from] = [];
+        }
+        routes[from].push(to);
+    }
+    
+    // 각 경로를 알파벳 순서로 정렬
+    for(const from in routes) {
+        routes[from].sort();
+    }
+    
+    
+    const stack = [];
+    const answer = [];
+    
+    function dfs(current) {
+        while(routes[current] && routes[current].length > 0) {
+            const next = routes[current].shift();
+            dfs(next);
+        }
+        answer.push(current);
+    }
+    
+    dfs("ICN");
+    
+    return answer.reverse();
+}
